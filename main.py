553de3386ff0a36847bdab0e5a1a5d7d82b3e6e4
engineer2loggie <engineer2loggie@gmail.com>
2025-08-21 17:46:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import sys
 # --- Configuration ---
 # Your list of EPG filenames from https://github.com/iptv-org/epg/blob/master/SITES.md
 IPTV_ORG_EPG_FILENAMES = [
-    "9tv.co.il.channels.xml",
-    "allente.dk.channels.xml",
+    "https://github.com/iptv-org/epg/blob/master/sites/9tv.co.il/9tv.co.il.channels.xml",
+    "https://github.com/iptv-org/epg/blob/master/sites/abc.net.au/abc.net.au_adl.channels.xml",
 ]
 
 # The base URL for IPTV-org EPG guides
