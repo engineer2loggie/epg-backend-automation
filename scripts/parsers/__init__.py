9ebaa29514efcf65df1e75bbf11aa2de8418f900
engineer2loggie <engineer2loggie@gmail.com>
2025-08-31 11:35:56 -0400

    Update __init__.py

--- scripts/parsers/__init__.py
+++ scripts/parsers/__init__.py
@@ -1,4 +1,8 @@
+# Register all available parsers here
 from .gatotv import GatoTVParser
-from .ontvtonight import OnTVTonightParser
+from .ontvtonight import OnTVTonightParser  # you already have this file
 
-ALL_PARSERS = [GatoTVParser(), OnTVTonightParser()]
+ALL_PARSERS = [
+    GatoTVParser(),
+    OnTVTonightParser(),  # preferred for MVSTV via OnTVTonight
+]
